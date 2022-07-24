4bc6ccb4d9bca25f015d47fb6f608a7092ba272c
byunsangGu <qustkdrn1@gmail.com>
2022-07-24 11:20:28 +0900

    test

---
+++ Main.js
@@ -0,0 +1,22 @@
+//시간 
+var Time = 3600;  
+var Min = ""; 
+var Sec = ""; 
+
+var x = setInterval(function(){ 
+    Min = parseInt(Time/60);  
+    Sec = Time % 60;  
+
+    document.getElementsByClassName("btn")[0].innerHTML = Min + "분" + Sec + "초";  
+    Time--; 
+
+    if(Time < 0) {
+        clearInterval(x);
+    }
+
+
+}, 1000); 
+
+document.getElementById('btn_date').value = new Date().toISOString().substring(0, 10);; // 날짜  
+
+
